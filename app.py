ecaa2d2e9538731ecf0ea81740c951e6d8892af6
Emmett Billiott <emmettbilliott@gmail.com>
2025-11-23 16:56:40 -0500

    Commit 4: Added a formatted output which displays the results of calculation

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ print("Welcome to my Python Program!")
 savings = input("How much money do you put in savings each month")
 savings = float(savings)
 yearly_savings = savings * 12
+print("At this rate, you will have $", yearly_savings, "in your savings in 12 months")
