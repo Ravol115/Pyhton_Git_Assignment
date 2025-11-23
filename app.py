00f4abd9c5a1f705c68adcc390b7fb40c3b15cf5
Emmett Billiott <emmettbilliott@gmail.com>
2025-11-23 16:53:30 -0500

    Commit 3: Added calculation based on User Input, and made certain fixes to previous commit

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 print("Welcome to my Python Program!")
-income = input("How much money do you put in savings each month")7
+savings = input("How much money do you put in savings each month")
+savings = float(savings)
+yearly_savings = savings * 12
