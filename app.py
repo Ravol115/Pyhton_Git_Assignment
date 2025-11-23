63717b79e66800f4d1e28fe3fb15c0b2c41d95b0
Emmett Billiott <emmettbilliott@gmail.com>
2025-11-23 16:59:18 -0500

    Commit 5: Added simple error handling for user input

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 print("Welcome to my Python Program!")
 savings = input("How much money do you put in savings each month")
-savings = float(savings)
+try:
+    savings = float(savings)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
 yearly_savings = savings * 12
 print("At this rate, you will have $", yearly_savings, "in your savings in 12 months")
